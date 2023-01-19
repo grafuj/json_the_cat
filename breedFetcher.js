5455f6d992615b5d147cd0259cc9f22c36c44b9e
Graham Fujisawa <sfrrki@gmail.com>
2023-01-19 15:08:04 -0800

    adding mocha test and rand eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,28 @@
 const request = require('request');
-const Breed = process.argv[2];
+// const Breed = process.argv[2];
 
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + Breed;
+const fetchBreedDescription = (breedName, callback) => {
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('error:', error);
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      // console.error('error:', error);
+      return callback(error);
+    }
+    
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback("That's not a breed!");
+      return;
+    }
+    
+    
+    // console.log(typeof data) //string, object after parsing
+    // console.log(data);
+    callback(null, data[0].description); //its in an array and we want property description
     return;
-  }
-
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("That's not a breed!");
-    return;
-  }
-
-  // console.log(typeof data) //string, object after parsing
-  // console.log(data);
-  console.log(data[0].description); //its in an array and we want property description
-
-});
+  });
+};
+  
+module.exports = {fetchBreedDescription};
