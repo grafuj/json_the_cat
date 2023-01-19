c74e9a32cf9515d7a21af6fdd6d572da2aed42a5
Graham Fujisawa <sfrrki@gmail.com>
2023-01-19 13:05:24 -0800

    finished solution for command line breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,22 @@
 const request = require('request');
+const Breed = process.argv[2];
+
+const url = "https://api.thecatapi.com/v1/breeds/search?q=" + Breed;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  if (data[0] === undefined) {
+    console.log("That's not a breed!");
+    return;
+  }
+
+  // console.log(typeof data) //string, object after parsing
+  // console.log(data);
+  console.log(data[0].description); //its in an array and we want property description
+
+});
